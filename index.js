f584d90842b8f129edf45608f97e036ea4fb1ed0
achingbrain <alex@achingbrain.net>
2014-12-05 14:17:14 +0000

    sensible defaults

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var Container = require('wantsit').Container,
   Columbo = require('columbo'),
   Hapi = require('hapi'),
-  path = require('path')
+  path = require('path'),
+  winston = require('winston')
 
 if(!process.env.BARBARA_BREW) {
   throw new Error('Please specify a brew id')
@@ -12,8 +13,16 @@ process.env.BARBARA_PORT = process.env.BARBARA_PORT || 7583
 process.env.BARBARA_FREQUENCY = process.env.BARBARA_FREQUENCY || 10000
 process.env.BARBARA_NOTIFICATION_INTERVAL = process.env.BARBARA_NOTIFICATION_INTERVAL || 500000
 process.env.BARBARA_TEMPERATURE_SENSOR = process.env.BARBARA_TEMPERATURE_SENSOR || '28-0000054c0b2f'
+process.env.BARBARA_SLOTS = process.env.BARBARA_SLOTS || '/sys/devices/bone_capemgr.9/slots'
 
 var container = new Container()
+container.createAndRegister('logger', winston.Logger, {
+  transports: [
+    new winston.transports.Console({
+      colorize: true
+    })
+  ]
+})
 container.register('restify', require('restify'))
 container.register('child_process', require('child_process'))
 
